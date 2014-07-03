9d5eadb4f95d52c99412d609daae9a2988f11642
Jahanzaib Aslam <jahanzaibaslam@hotmail.com>
2014-07-03 15:34:30 +0500

    Small UI Fixups

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.configure(function () {
 
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8088);
 app.set('views', path.join(__dirname, 'public/theme/views'));
 app.set('view engine', 'ejs');
 app.use(express.favicon());
