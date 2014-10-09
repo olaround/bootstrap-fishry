efa0c86dfad8b2dbc4849ab9110b8b1c5ce13208
Jahanzaib Aslam <jahanzaibaslam@hotmail.com>
2014-10-09 16:17:44 +0500

    Darson theme

---
+++ public/themes/darson/1409815361543/base/js/controllers/home.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+function HomeCtrl($scope,$location,$rootScope){
+	$rootScope.locationParam = [];
+	var locationsParam = $scope.$location.path().split('/');	
+	$.each(locationsParam,function(index,item){
+		if(item && item != ''){
+			$rootScope.locationParam.push(item);
+		}
+	});
+	$rootScope.title = $rootScope.SettingGeneral.settings.meta_title;
+}
